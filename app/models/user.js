795d91c47b60023b9a3f3128fc995714a85ac4a2
jvo978 <jasonv.vo@gmail.com>
2018-07-17 11:48:02 -0400

    Added company into userSchema with type: String and required: false.

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: false
+  },
   email: {
     type: String,
     required: true,
