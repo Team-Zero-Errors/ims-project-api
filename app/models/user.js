5e9d0965fb4b1fea9376c9ff4fbfb76d281363a0
KatherineWard <katherine.eileen.ward@gmail.com>
2018-07-18 09:32:31 -0400

    Removed company field from model.js to match with client side

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  company: {
-    type: String,
-    required: false
-  },
   email: {
     type: String,
     required: true,
